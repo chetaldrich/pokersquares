505542b2efcefedf06db36ed3d1d35b7db542516
Danny Hanson <hansond@carleton.edu>
2015-04-27 21:02:02 -0500

    Changing Rule to make it better

--- src/Rule.java
+++ src/Rule.java
@@ -1,13 +1,26 @@
-
+import java.util.Random;
 
 public class Rule {
     
     Card[][] grid = new Card[5][5];
+    boolean row;
+    boolean column;
+    int rank;
+    String suit;
 
     public Rule() {
         for (int row = 0; row < 5; row++)
             for (int col = 0; col < 5; col++)
                 grid[row][col] = null;
+        String[] possibleSuits = ["hearts","spades","clubs","diamonds"];
+        boolean randBool = nextBoolean();
+        row = randBool;
+        column = !row;
+        int randInt = nextInt(13);
+        rank = 
+        randInt = nextInt(4);
+        suit = possibleSuits[randInt];
+
     }
 
     private int checkRow(int rowNum) {
@@ -16,6 +29,15 @@ public class Rule {
 
     private int checkColumn(int colNum) {
         return 1;
+    }
+
+    public dosomething(Card[][] grid) {
+        this.grid = grid;
+        if (row) {
+            checkRow(rownumber)
+        } else if (column) {
+            checkColumn(colnumber)
+        }
     }
 
 }
