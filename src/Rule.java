8420848f53dc866b987f873d69d2c1f14a4a0326
Danny Hanson <hansond@carleton.edu>
2015-04-27 20:35:06 -0500

    Minor updates to Chromosome and Rule

--- src/Rule.java
+++ src/Rule.java
@@ -1,9 +1,21 @@
 
 
 public class Rule {
+    
+    Card[][] grid = new Card[5][5];
 
-	public Rule() {
+    public Rule() {
+        for (int row = 0; row < 5; row++)
+            for (int col = 0; col < 5; col++)
+                grid[row][col] = null;
+    }
 
-	}
+    private boolean checkRow(int rowNum) {
+    
+    }
+
+    private boolean checkColumn(int colNum) {
+    
+    }
 
 }
